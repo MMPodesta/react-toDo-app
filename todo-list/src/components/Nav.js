e04f1dbd2f7dd185aee162d3bf1a06cf10629afb
MauricioMurta <mauriciomurta13@hotmail.com>
2023-01-30 19:54:44 +0000

    login and logout feature

--- todo-list/src/components/Nav.js
+++ todo-list/src/components/Nav.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Nav = () =>{
+    const navigate = useNavigate();
+
+    const logOut = () =>{
+        localStorage.clear();
+        navigate("/");
+    };
+
     return(
         <div>
             <h1>ToDo List</h1>
-            <Link to="/"> Home </Link>
-            <Link to="/profile"> Profile </Link> 
+            <h2>
+                <Link to="/"> Home </Link>
+                <Link to="/profile"> Profile </Link> 
+                <button onClick={logOut}>Sign Out</button>
+            </h2>      
         </div>
     );
 };
--- todo-list/src/pages/Home.js
+++ todo-list/src/pages/Home.js
@@ -1,10 +1,25 @@
 import { signInWithGoogle } from "../Firebase";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () =>{
+    const navigate = useNavigate();
+    
+    const login = () =>{
+        signInWithGoogle().then((result) => {
+            const name = result.user.displayName;
+            localStorage.setItem("name", name);
+            navigate('/profile');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        
+    }
+
     return(
         <div>
             <h1> Home Page</h1>
-            <button onClick={signInWithGoogle}> Sign In With Google</button>
+            <button onClick={login}> Sign In With Google</button>
         </div> 
     );
 };
