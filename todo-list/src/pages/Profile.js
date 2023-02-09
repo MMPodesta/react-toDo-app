8614140f137df051aa0a7c5b006b308b3d70d124
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-09 15:57:02 +0000

    Feature to clear input field after submit

--- todo-list/src/pages/Profile.js
+++ todo-list/src/pages/Profile.js
@@ -20,6 +20,7 @@ export const Profile = () =>{
     //add item and update
     const createItem = async () =>{
         await addDoc(userCollectionRef, { item: newItem });
+        setNewItem("");
         getUsers();
     }
 
@@ -41,7 +42,7 @@ export const Profile = () =>{
             <Greeting name={localStorage.getItem("name")}/>
             <p className="text-lg">Add, check and delete your tasks below.</p>
             <div className="flex mt-5 w-96 justify-between">
-                <input maxlength="20" required className=" bg-gray-100 flex-1 py-2 px-4 "
+                <input maxlength="20" value={newItem} className=" bg-gray-100 flex-1 py-2 px-4 "
                 placeholder="Task..." 
                 onChange={(event) => {
                     setNewItem(event.target.value);
