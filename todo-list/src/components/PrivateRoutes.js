6ba08198a5cb6a16a58e2f17c7c6774310390e4a
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-10 18:23:01 +0000

    Comment

--- todo-list/src/components/PrivateRoutes.js
+++ todo-list/src/components/PrivateRoutes.js
@@ -3,6 +3,7 @@ import { Outlet, Navigate } from "react-router-dom"
 export const PrivateRoutes = () => {
     
     return(
+        // this makes an 404 error when refreshing private page 
         localStorage.getItem("name") ? <Outlet/> : <Navigate to='/'/>
     );
 };
